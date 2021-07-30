1d19a1daddbe7bc8f94a3974085ce3a65a055313
XinZhang2594249Z <2594249Z@student.gla.ac.uk>
2021-07-30 12:56:55 +0100

    Creating User View and Template

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -79,6 +79,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -87,6 +88,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 LANGUAGE_CODE = 'en-us'
 
