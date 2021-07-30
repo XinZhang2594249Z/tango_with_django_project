edf29fa66f83b68f2b085178509e9711476d144c
XinZhang2594249Z <2594249Z@student.gla.ac.uk>
2021-07-30 16:34:11 +0100

    check 2

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 app_name = 'rango'
 
 urlpatterns = [
-    
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
@@ -14,6 +13,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
-    path('logout/', views.user_logout, name='logout'),
+    path('logout/', views.user_logout, name='logout'),tango_with_django_project
 ]
 
